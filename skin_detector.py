7e00a058746f55759dcf244c871d146d304fae65
igorcrexito <igorcrexito@gmail.com>
2020-06-27 09:21:23 -0300

    Update skin_detector.py

--- skin_detector.py
+++ skin_detector.py
@@ -12,35 +12,40 @@ import utils.metrics as metrics
 #defining some hyperparameters/parameters
 width = 224
 height = 224
-batch_size = 6
+batch_size = 10
 channels = 3
-number_of_epochs = 40
+number_of_epochs = 10
+number_of_training_steps = 1
+number_of_loops = 1
 
 #supportive images
 supportive_width = 14
 supportive_height = 14
 supportive_channels = 3
 max_number_of_supportive_images = 1024
-augmentation = 0
+augmentation = 1
 
 #base paths to load data
 Pratheephan_path = 'dataset/Pratheepan/'
 ECU_path = 'dataset/ECU/'
 HGR_path = 'dataset/HGR/'
 support_path = 'dataset/support_dataset/'
+He_training_path = 'dataset/He/training/'
+He_validation_path = 'dataset/He/validation/'
+
 
 if __name__ == "__main__":
-    
-    print('--- Reading input data ---')
-    rgb_sequence, hsv_sequence, ycrcb_sequence, output_sequence, background_sequence = fr.read_training_files(ECU_path, width, height, augmentation)
-    
-    print('--- Reading support dataset ---')
-    skin_rgb_sequence, skin_hsv_sequence, skin_ycrcb_sequence, no_skin_rgb_sequence, no_skin_hsv_sequence, no_skin_ycrcb_sequence = fr.read_supporting_files(support_path, supportive_width, supportive_height, max_number_of_supportive_images)
+
+    #loading models
+    print('--- Loading pre-trained models ---')
+    cust = {'InstanceNormalization': InstanceNormalization}
+    autoencoder_skin = load_model("model_" + "skin" + ".h5", cust)
+    autoencoder_background = load_model("model_" + "background" + ".h5", cust)
     
     #assembling skin detector autoencoder
-    print('--- Assembling a skin detector autoencoder')
-    autoencoder_skin = model.create_skin_detector_model(width, height, channels, supportive_width, supportive_height, supportive_channels, max_number_of_supportive_images)
-    autoencoder_background = model.create_skin_detector_model(width, height, channels, supportive_width, supportive_height, supportive_channels, max_number_of_supportive_images)
+    #print('--- Assembling a skin detector autoencoder')
+    #autoencoder_skin = model.create_skin_detector_model(width, height, channels, supportive_width, supportive_height, supportive_channels, max_number_of_supportive_images)
+    #autoencoder_background = model.create_skin_detector_model(width, height, channels, supportive_width, supportive_height, supportive_channels, max_number_of_supportive_images)
     
     #creating a composite model to correlate both autoencoders
     print('--- Assembling composite model')
@@ -48,9 +53,21 @@ if __name__ == "__main__":
     support_image_shape = (max_number_of_supportive_images, supportive_width, supportive_height, supportive_channels)
     composite_model, skin_model, background_model = model.define_composite_model(autoencoder_background, autoencoder_skin, image_shape, support_image_shape)
     
-    #fitting the model -> comment the subsequent lines to not execute the training step
-    print('--- Training model with original images and binary masks ---')
-    #skin_model, background_model = model.train_skin_background_models(composite_model, skin_model, background_model, rgb_sequence, hsv_sequence, ycrcb_sequence, skin_hsv_sequence, no_skin_hsv_sequence, output_sequence, background_sequence, width, height, channels, batch_size, number_of_epochs)
+    ############################## READING TRAINING DATA ##############################################################################
+    print('--- Reading support dataset ---')
+    skin_rgb_sequence, skin_hsv_sequence, skin_ycrcb_sequence, no_skin_rgb_sequence, no_skin_hsv_sequence, no_skin_ycrcb_sequence = fr.read_supporting_files(support_path, supportive_width, supportive_height, max_number_of_supportive_images)
+    
+    #comment these lines if you just want to load the models
+    '''
+    for outer_loop in range(0, number_of_loops): #simula um generator mas permite carregar mais dados de vez
+        for current_step in range(0, number_of_training_steps):
+            print('--- Reading input data for step '+ str(current_step)+ ' ---')
+            rgb_sequence, hsv_sequence, ycrcb_sequence, output_sequence, background_sequence = fr.read_training_files(He_training_path, width, height, number_of_training_steps, current_step, augmentation)
+
+            #fitting the model -> comment the subsequent lines to not execute the training step
+            print('--- Training model with original images and binary masks ---')
+            skin_model, background_model = model.train_skin_background_models(composite_model, skin_model, background_model, rgb_sequence, hsv_sequence, ycrcb_sequence, skin_hsv_sequence, no_skin_hsv_sequence, output_sequence, background_sequence, width, height, channels, batch_size, number_of_epochs)
+    '''
     
     #predicting in a different dataset
     print('--- Reading test data ---')
@@ -73,6 +90,7 @@ if __name__ == "__main__":
         #cv2.imshow('background',output_background[0])
         #cv2.imshow('skin',output_skin[0])
         #cv2.imshow('rgb_image',rgb_sequence[i])
+        #cv2.waitKey(0);                                          
         
         skin_image = output_skin[0]*255
         groundtruth_image = output_sequence[i]
@@ -81,7 +99,6 @@ if __name__ == "__main__":
         print('-----------------------------------------------------------')
         print('IoU: ' + str(IoU))
         
-        #cv2.waitKey(0);                                          
         averageIoU+= IoU
         
-    print('Average IoU: '+ str(averageIoU/len(rgb_sequence)))                       
+    print('Average IoU: '+ str(averageIoU/len(rgb_sequence)))
